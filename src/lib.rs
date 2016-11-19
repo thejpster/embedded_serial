3a6ecdb09f347eef45dbb121c294a752bb24e92b
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2016-11-19 14:02:11 +0000

    Switch to associated types.

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,60 @@
 #![no_std]
 #![feature(never_type)]
 
-pub trait BlockingTx<E> {
+pub trait BlockingTx {
+    type Error;
+
     /// Write a single octet to the port's transmitter,
     /// blocking until the character can be stored in the buffer
     /// (not necessarily that the character has been transmitted).
-    fn putc(&mut self, ch: u8) -> Result<(), E>;
+    ///
+    /// In some implementations, this can result in an Error.
+    /// If not, use `type Error = !`.
+    fn putc(&mut self, ch: u8) -> Result<(), Self::Error>;
 }
 
-pub trait NonBlockingTx<E> {
+pub trait NonBlockingTx {
+    type Error;
+
     /// Try and write a single octet to the port's transmitter.
-    /// Will return Err(()) if the FIFO/buffer was full
+    /// Will return `Err` if the FIFO/buffer was full
     /// and the character couldn't be stored.
-    fn putc_try(&mut self, ch: u8) -> Result<(),E>;
+    ///
+    /// In some implementations, this can result in an Error.
+    /// If not, use `type Error = !`.
+    fn putc_try(&mut self, ch: u8) -> Result<(), Self::Error>;
 }
 
-pub trait BlockingRx<E> {
+pub trait BlockingRx {
+    type Error;
+
     /// Read a single octet from the port's receiver,
     /// blocking until the character can be read from the buffer.
-    fn getc(&mut self) -> Result<u8, E>;
+    ///
+    /// In some implementations, this can result in an Error.
+    /// If not, use `type Error = !`.
+    fn getc(&mut self) -> Result<u8, Self::Error>;
 }
 
-pub trait BlockingRxWithTimeout<T, E> {
+pub trait BlockingRxWithTimeout {
+    type Timeout;
+    type Error;
+
     /// Read a single octet from the port's receiver,
     /// blocking until the character can be read from the buffer.
-    fn getc(&mut self, timeout: T) -> Result<u8, E>;
+    ///
+    /// In some implementations, this can result in an Error.
+    /// If not, use `type Error = !`.
+    fn getc(&mut self, timeout: Self::Timeout) -> Result<u8, Self::Error>;
 }
 
-pub trait NonBlockingRx<E> {
+pub trait NonBlockingRx {
+    type Error;
+
     /// Read a single octet from the port's receiver; if the buffer
     /// is empty 
-    fn getc_try(&mut self) -> Result<u8, E>;
+    ///
+    /// In some implementations, this can result in an Error.
+    /// If not, use `type Error = !`.
+    fn getc_try(&mut self) -> Result<u8, Self::Error>;
 }
